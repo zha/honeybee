febbed4ad896bac9c9b80c5c7d0c771c20533c62
shzhan <szhan.me@gmail.com>
2020-05-17 16:07:39 -0400

    Some updates

--- honeybeeradiance/run_rad.py
+++ honeybeeradiance/run_rad.py
@@ -1,7 +1,10 @@
 import subprocess
 import os
+import honeybeeradiance.config as config
+
 def run_rad(cmd_rad, working_folder):
     if os.name == 'nt':
+        os.environ['RAYPATH'] += ';%s' % os.path.normpath(config.radlib_path)
         process = subprocess.Popen('cmd', cwd = working_folder, shell = True, universal_newlines=True,stdin =subprocess.PIPE, stdout =subprocess.PIPE)
         out, err = process.communicate(cmd_rad)
     else:
